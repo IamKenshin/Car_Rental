e774e57ec7abb5ed0095440438fdd4a283ee38c5
Aayush Neupane <aayush_alex@yahoo.com>
2016-11-30 18:05:29 -0800

    Update car_rental.sql
    added auto_increment

--- sql_scripts/car_rental.sql
+++ sql_scripts/car_rental.sql
@@ -3,15 +3,19 @@ USE `carrental`;
 
 DROP TABLE IF EXISTS `agency`;
 CREATE TABLE IF NOT EXISTS `agency` (
-  `aID` int(11) NOT NULL PRIMARY KEY,
+  `aID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Address` varchar(255) NOT NULL,
   `City` varchar(255) NOT NULL,
   `Zip` int(11) NOT NULL
 );
 
+INSERT INTO `agency` (`Address`, `City`, `Zip`) VALUES
+('123 Main St', 'San Jose', 95192);
+
+
 DROP TABLE IF EXISTS `car`;
 CREATE TABLE IF NOT EXISTS `car` (
-  `cID` int(11) NOT NULL PRIMARY KEY,
+  `cID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Make` varchar(255) NOT NULL,
   `Model` varchar(255) NOT NULL,
   `Year` int(11) NOT NULL,
@@ -24,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `car` (
 
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
-  `uid` int(11) NOT NULL PRIMARY KEY,
+  `uid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `fname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
   `age` int(10) NOT NULL,
@@ -36,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 
 DROP TABLE IF EXISTS `rental`;
 CREATE TABLE IF NOT EXISTS `rental` (
-  `ContractNumber` int(11) NOT NULL PRIMARY KEY,
+  `ContractNumber` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Customer` int(11) NOT NULL, FOREIGN KEY (Customer) REFERENCES customer(uid),
   `Car` int(11) NOT NULL, FOREIGN KEY (Car) REFERENCES car(cid),
   `Agency` int(11) NOT NULL, FOREIGN KEY(Agency) REFERENCES agency(aid),
@@ -50,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `rental` (
 
 DROP TABLE IF EXISTS `reservation`;
 CREATE TABLE IF NOT EXISTS `reservation` (
-  `ReservationNumber` int(11) NOT NULL PRIMARY KEY,
+  `ReservationNumber` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Customer` int(11) NOT NULL, FOREIGN KEY (Customer) REFERENCES customer(uID),
   `Agency` int(11) NOT NULL, FOREIGN KEY (Agency) REFERENCES agency(aID),
   `StartDate` date NOT NULL,
