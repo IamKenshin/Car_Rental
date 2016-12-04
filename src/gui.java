7be94bd7b3d8166575c5fa7587d0128fe597def1
aayush n <aayush_Alex@yahoo.com>
2016-12-04 14:16:47 -0800

    added view and modify buttons that populate customer info and modify db accordingly

--- src/gui.java
+++ src/gui.java
@@ -37,6 +37,7 @@ public class gui {
 	private JTextField car_id_text;
 	private JTextField car_price_text;
 	private JTextField car_type_text;
+	private JButton btnView;
 	
 			
 
@@ -87,188 +88,189 @@ public class gui {
 				customer_id_text.setText(String.valueOf(customerId));
 				
 				JOptionPane.showMessageDialog(null, fName + lName + age);
-//				int customerId, age;
-//				String fName, lName, licenceNumber, ccNumber, customerStatus;
-				//JOptionPane.showMessageDialog(null, "Customer Successfully added!");
 			}
 		});
 		
 		JButton customer_modify_button = new JButton("Modify");
 		customer_modify_button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String fName = customer_fName_text.getText();
-				String lName = customer_lName_text.getText();
-				int age = Integer.parseInt(customer_age_text.getText());
-				String licenceNumber = customer_licNum_text.getText();
-				String ccNumber = customer_ccNum_text.getText();
-				String customerStatus = "Reservation"; 				//customer is going to be reservation status in the beginning
-				int customerID = Integer.parseInt(customer_id_text.getText());
-				Customer customer = new Customer(fName, lName, age, licenceNumber, ccNumber, customerStatus);
-				dao.updateCustomer(customerID, customer);
+			public void actionPerformed(ActionEvent arg0) {	
+				String fName, lName, licenceNumber, ccNumber, customerStatus=null;
+				int age = 0, customerId;
+				
+				fName = customer_fName_text.getText();
+				lName = customer_lName_text.getText();
+				age = Integer.parseInt(customer_age_text.getText());
+				licenceNumber = customer_licNum_text.getText();
+				ccNumber = customer_ccNum_text.getText();
+				customerId = Integer.parseInt(customer_id_text.getText());
+			
+				Customer newCustomer = new Customer(fName, lName, age, licenceNumber, ccNumber, customerStatus);
+				dao.updateCustomer(customerId, newCustomer);
 				
 				JOptionPane.showMessageDialog(null, fName + lName + age);
 			}
 		});
-		customer_add_button.setBounds(23, 249, 73, 23);
+		customer_add_button.setBounds(10, 249, 74, 23);
 		frame.getContentPane().add(customer_add_button);
 		
-		customer_modify_button.setBounds(103, 249, 89, 23);
+		customer_modify_button.setBounds(174, 249, 62, 23);
 		frame.getContentPane().add(customer_modify_button);
+		customer_modify_button.setEnabled(false);
 		
 		customer_fName_text = new JTextField();
-		customer_fName_text.setBounds(106, 32, 86, 20);
+		customer_fName_text.setBounds(106, 32, 130, 20);
 		frame.getContentPane().add(customer_fName_text);
 		customer_fName_text.setColumns(10);
 		
 		customer_lName_text = new JTextField();
-		customer_lName_text.setBounds(106, 63, 86, 20);
+		customer_lName_text.setBounds(106, 63, 130, 20);
 		frame.getContentPane().add(customer_lName_text);
 		customer_lName_text.setColumns(10);
 		
 		customer_age_text = new JTextField();
-		customer_age_text.setBounds(106, 94, 86, 20);
+		customer_age_text.setBounds(106, 94, 130, 20);
 		frame.getContentPane().add(customer_age_text);
 		customer_age_text.setColumns(10);
 		
 		customer_licNum_text = new JTextField();
-		customer_licNum_text.setBounds(106, 125, 86, 20);
+		customer_licNum_text.setBounds(106, 125, 130, 20);
 		frame.getContentPane().add(customer_licNum_text);
 		customer_licNum_text.setColumns(10);
 		
 		customer_ccNum_text = new JTextField();
-		customer_ccNum_text.setBounds(106, 156, 86, 20);
+		customer_ccNum_text.setBounds(106, 156, 130, 20);
 		frame.getContentPane().add(customer_ccNum_text);
 		customer_ccNum_text.setColumns(10);
 		
 		customer_status_text = new JTextField();
 		customer_status_text.setText("Reservation");
-		customer_status_text.setBounds(106, 186, 86, 20);
+		customer_status_text.setBounds(106, 186, 130, 20);
 		frame.getContentPane().add(customer_status_text);
 		customer_status_text.setColumns(10);
 		customer_status_text.setEditable(false);
 		
 		customer_id_text = new JTextField();
-		customer_id_text.setBounds(106, 217, 86, 20);
+		customer_id_text.setBounds(106, 217, 130, 20);
 		frame.getContentPane().add(customer_id_text);
 		customer_id_text.setColumns(10);
 		
 		JLabel customer_status_label = new JLabel("Status");
-		customer_status_label.setBounds(23, 189, 46, 14);
+		customer_status_label.setBounds(23, 189, 213, 14);
 		frame.getContentPane().add(customer_status_label);
 		
 		JLabel customer_fName_label = new JLabel("First Name");
-		customer_fName_label.setBounds(23, 35, 73, 14);
+		customer_fName_label.setBounds(23, 35, 213, 14);
 		frame.getContentPane().add(customer_fName_label);
 		
 		JLabel customer_lName_label = new JLabel("Last Name");
-		customer_lName_label.setBounds(23, 66, 73, 14);
+		customer_lName_label.setBounds(23, 66, 213, 14);
 		frame.getContentPane().add(customer_lName_label);
 		
 		customer_age_label = new JLabel("Age");
-		customer_age_label.setBounds(23, 97, 46, 14);
+		customer_age_label.setBounds(23, 97, 213, 14);
 		frame.getContentPane().add(customer_age_label);
 		
 		customer_licNum_label = new JLabel("Licence");
-		customer_licNum_label.setBounds(23, 128, 65, 14);
+		customer_licNum_label.setBounds(23, 128, 213, 14);
 		frame.getContentPane().add(customer_licNum_label);
 		
 		customer_ccNum_label = new JLabel("CC Number");
-		customer_ccNum_label.setBounds(23, 159, 73, 14);
+		customer_ccNum_label.setBounds(23, 159, 213, 14);
 		frame.getContentPane().add(customer_ccNum_label);
 		
 		customer_id_label = new JLabel("ID");
-		customer_id_label.setBounds(23, 220, 46, 14);
+		customer_id_label.setBounds(23, 220, 213, 14);
 		frame.getContentPane().add(customer_id_label);		
 		
 		JLabel customer_label = new JLabel("Customer");
 		customer_label.setFont(new Font("Tahoma", Font.BOLD, 13));
-		customer_label.setBounds(63, 7, 113, 14);
+		customer_label.setBounds(63, 7, 173, 14);
 		frame.getContentPane().add(customer_label);
 		
 		JLabel lblCar = new JLabel("Car");
-		lblCar.setBounds(327, 8, 46, 14);
+		lblCar.setBounds(379, 8, 74, 14);
 		frame.getContentPane().add(lblCar);
 		
 		JLabel car_year_label = new JLabel("Year");
-		car_year_label.setBounds(223, 35, 46, 14);
+		car_year_label.setBounds(275, 35, 178, 14);
 		frame.getContentPane().add(car_year_label);
 		
 		JLabel car_make_label = new JLabel("Make");
-		car_make_label.setBounds(223, 66, 46, 14);
+		car_make_label.setBounds(275, 66, 178, 14);
 		frame.getContentPane().add(car_make_label);
 		
 		JLabel car_model_label = new JLabel("Model");
-		car_model_label.setBounds(223, 97, 46, 14);
+		car_model_label.setBounds(275, 97, 178, 14);
 		frame.getContentPane().add(car_model_label);
 		
 		JLabel car_mileage_label = new JLabel("Mileage");
-		car_mileage_label.setBounds(223, 128, 46, 14);
+		car_mileage_label.setBounds(275, 128, 178, 14);
 		frame.getContentPane().add(car_mileage_label);
 		
 		JLabel car_condition_label = new JLabel("Condition");
-		car_condition_label.setBounds(223, 162, 46, 14);
+		car_condition_label.setBounds(275, 162, 178, 14);
 		frame.getContentPane().add(car_condition_label);
 		
 		JLabel car_status_label = new JLabel("Status");
-		car_status_label.setBounds(223, 189, 46, 14);
+		car_status_label.setBounds(275, 189, 178, 14);
 		frame.getContentPane().add(car_status_label);
 		
 		JLabel car_id_label = new JLabel("ID");
-		car_id_label.setBounds(223, 223, 46, 14);
+		car_id_label.setBounds(275, 223, 178, 14);
 		frame.getContentPane().add(car_id_label);
 		
 		JLabel car_price_label = new JLabel("Price");
-		car_price_label.setBounds(223, 249, 46, 14);
+		car_price_label.setBounds(275, 253, 178, 14);
 		frame.getContentPane().add(car_price_label);
 		
 		JLabel car_type_label = new JLabel("Type");
-		car_type_label.setBounds(223, 269, 46, 14);
+		car_type_label.setBounds(275, 284, 178, 14);
 		frame.getContentPane().add(car_type_label);
 		
 		car_year_text = new JTextField();
-		car_year_text.setBounds(287, 32, 86, 20);
+		car_year_text.setBounds(339, 32, 114, 20);
 		frame.getContentPane().add(car_year_text);
 		car_year_text.setColumns(10);
 		
 		car_make_text = new JTextField();
-		car_make_text.setBounds(287, 63, 86, 20);
+		car_make_text.setBounds(339, 63, 114, 20);
 		frame.getContentPane().add(car_make_text);
 		car_make_text.setColumns(10);
 		
 		car_model_text = new JTextField();
-		car_model_text.setBounds(287, 94, 86, 20);
+		car_model_text.setBounds(339, 94, 114, 20);
 		frame.getContentPane().add(car_model_text);
 		car_model_text.setColumns(10);
 		
 		car_mileage_text = new JTextField();
-		car_mileage_text.setBounds(287, 125, 86, 20);
+		car_mileage_text.setBounds(339, 125, 114, 20);
 		frame.getContentPane().add(car_mileage_text);
 		car_mileage_text.setColumns(10);
 		
 		car_condition_text = new JTextField();
-		car_condition_text.setBounds(287, 156, 86, 20);
+		car_condition_text.setBounds(339, 156, 114, 20);
 		frame.getContentPane().add(car_condition_text);
 		car_condition_text.setColumns(10);
 		
 		car_status_text = new JTextField();
 		car_status_text.setText("On Hand");
-		car_status_text.setBounds(287, 186, 86, 20);
+		car_status_text.setBounds(339, 186, 114, 20);
 		frame.getContentPane().add(car_status_text);
 		car_status_text.setColumns(10);
 		car_status_text.setEditable(false);
 		
 		car_id_text = new JTextField();
-		car_id_text.setBounds(287, 217, 86, 20);
+		car_id_text.setBounds(339, 217, 114, 20);
 		frame.getContentPane().add(car_id_text);
 		car_id_text.setColumns(10);
 		
 		car_price_text = new JTextField();
-		car_price_text.setBounds(287, 240, 86, 20);
+		car_price_text.setBounds(339, 250, 114, 20);
 		frame.getContentPane().add(car_price_text);
 		car_year_text.setColumns(10);
 		
 		car_type_text = new JTextField();
-		car_type_text.setBounds(287, 270, 86, 20);
+		car_type_text.setBounds(339, 281, 114, 20);
 		frame.getContentPane().add(car_type_text);
 		car_type_text.setColumns(10);
 		
@@ -281,10 +283,10 @@ public class gui {
 				JOptionPane.showMessageDialog(null, car_make_text.getText() + car_model_text.getText() + Integer.parseInt(car_year_text.getText()) + " added.");
 			}
 		});
-		car_add_button.setBounds(223, 300, 57, 23);
+		car_add_button.setBounds(275, 320, 64, 23);
 		frame.getContentPane().add(car_add_button);
 		
-		JButton car_modify_button = new JButton("Modify");
+		JButton car_modify_button = new JButton("Mod");
 		car_modify_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Car c = new Car(0, car_make_text.getText(), car_model_text.getText(), Integer.parseInt(car_year_text.getText()), Integer.parseInt(car_mileage_text.getText()), car_condition_text.getText(), car_type_text.getText(), Integer.parseInt(car_price_text.getText()), car_status_text.getText());
@@ -294,8 +296,39 @@ public class gui {
 
 			}
 		});
-		car_modify_button.setBounds(297, 300, 76, 23);
+		car_modify_button.setBounds(391, 320, 62, 23);
 		frame.getContentPane().add(car_modify_button);
+		
+		JButton customer_view_button = new JButton("View");
+		customer_view_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String oldfName = null, oldlName = null, oldLicenceNumber = null, oldCCNumber = null;
+				int oldAge = 0;
+				
+				int customerID = Integer.parseInt(customer_id_text.getText());
+				Customer customer = dao.searchCustomer(customerID);
+				oldfName = customer.getFname();
+				oldlName = customer.getLname();
+				oldAge = customer.getAge();
+				oldLicenceNumber = customer.getLicenceNumber();
+				oldCCNumber = customer.getCcNumber();
+				
+				customer_fName_text.setText(oldfName);
+				customer_lName_text.setText(oldlName);
+				customer_age_text.setText(String.valueOf(oldAge));
+				customer_licNum_text.setText(oldLicenceNumber);
+				customer_ccNum_text.setText(oldCCNumber);
+				
+				customer_id_text.setEditable(false);
+				customer_modify_button.setEnabled(true);
+			}
+		});
+		customer_view_button.setBounds(94, 249, 70, 23);
+		frame.getContentPane().add(customer_view_button);
+		
+		btnView = new JButton("View");
+		btnView.setBounds(349, 320, 32, 23);
+		frame.getContentPane().add(btnView);
 		
 	}
 }
