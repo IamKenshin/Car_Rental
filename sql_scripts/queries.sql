21eed0841f4486ef3a5b895ff1991cd200eedfea
michael's mac <enehnnamudi@gmail.com>
2016-12-04 10:22:04 -0800

    completed the queries

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -22,9 +22,35 @@ SELECT COUNT(*)
  SELECT AVG(Year)
   FROM car;
 
+/* shows the prices of each car*/
+ SELECT make, model, year, price 
+ FROM car;
+
 /* selects all customers and their rental*/
  SELECT customer.lname, customer.fname, rental.ContractNumber
 FROM customer 
 FUll OUTER JOIN rental
 ON customer.uid=rental.Customer
 ORDER BY customer.lname;
+
+/* pairs each rented car with the customer and the age of the oldest renter of that car*/
+select distinct car.make, car.model, car.year, lname, fname, age
+from customer, car, rental
+where car.cid=rental.car and customer.uid = rental.customer
+and age >= all
+(select age
+from customer, rental
+where customer.uid = rental.customer and car.cid =rental.car and
+age is not null);
+
+/* selects list of cars that has not yet been rented*/
+SELECT * FROM car
+WHERE NOT EXISTS
+(SELECT * FROM rental WHERE rental.car = car.cid);
+
+/* selects list of cars that has been rented*/
+SELECT * FROM car 
+WHERE EXISTS (SELECT * FROM rental
+WHERE rental.car=car.cid);
+
+
