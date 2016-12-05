18d0628fdcf4532cbd0be1f7c16c7b11faf7bd92
aayush n <aayush_Alex@yahoo.com>
2016-12-05 15:27:37 -0800

    fixed exlipse package

--- src/gui.java
+++ src/gui.java
@@ -73,7 +73,7 @@ public class gui {
 			
 
 	/**
-	 * Launch the application.
+	 * Launch the application
 	 */
 	public static void main(String[] args) {	
 		EventQueue.invokeLater(new Runnable() {
